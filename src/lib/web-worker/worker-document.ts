a34be8794d575da00b5d3274a7300845e58b19d6
gioboa <giorgiob.boa@gmail.com>
2024-11-16 20:21:35 +0100

    fix: fix up imports type

--- src/lib/web-worker/worker-document.ts
+++ src/lib/web-worker/worker-document.ts
@@ -13,7 +13,6 @@ import {
   getOrCreateNodeInstance,
 } from './worker-constructors';
 import { createEnvironment } from './worker-environment';
-import { createWindow } from './worker-window';
 import { debug, definePrototypePropertyDescriptor, randomId, SCRIPT_TYPE } from '../utils';
 import { ABOUT_BLANK, elementStructurePropNames, IS_TAG_REG, WinIdKey } from './worker-constants';
 import { getInstanceStateValue } from './worker-state';
